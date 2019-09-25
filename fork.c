bdeba70092f3823775efbe8ae9a6c712ef7efb1f
muhammadbilalakbar021 <muhammadbilalakbar021@gmail.com>
2019-09-26 02:01:41 +0500

    Fork child

---
+++ fork.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+#include <unistd.h>
+
+int main( void )
+{
+    printf( "parent pid %d\n", getpid() );
+
+    for ( int i = 0; i < 3; i++ )
+        if ( fork() == 0 )
+        {
+            printf( "[son] pid %d from pid %d\n", getpid(), getppid() );
+            exit( 0 );
+        }
+
+    for ( int i = 0; i < 3; i++ )
+        wait( NULL );
+}
